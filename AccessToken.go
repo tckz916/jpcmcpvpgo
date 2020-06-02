5bb96b008f1aa6d72722a5caf6375304e4269335
hayatarou_ <tachikaze0916@gmail.com>
2020-06-02 20:48:22 +0900

    AccessToken.go

--- AccessToken.go
+++ AccessToken.go
@@ -1,12 +1,50 @@
 package jpmcpvpapi
 
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+var BaseURL string = "https://minecraft.jp/oauth/token"
+
+type TokenRequest struct {
+	GrantType    string `json:"grant_type"`
+	ClientId     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
 type ClientCredentials struct {
-	AccessToken string      `json:"access_token"`
-	ExpiresIn   int         `json:"expires_in"`
-	TokenType   string      `json:"token_type"`
-	Scope       interface{} `json:"scope"`
+	AccessToken string `json:"access_token"`
+	ExpiresIn   string `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+	Scope       string `json:"scope"`
 }
 
 func GetAccessToken(client_id string, client_secret string) (client *ClientCredentials, err error) {
+	tokenRequest := TokenRequest{"client_credentials", client_id, client_secret}
+	b, err := json.Marshal(tokenRequest)
 
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(BaseURL, "application/json", bytes.NewBuffer(b))
+
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(body, &client)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
